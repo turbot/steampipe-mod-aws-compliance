924eb5214427b94332411ec6db99b3118936083b
cbruno10 <cody@turbot.com>
2021-06-02 18:06:05 -0400

    Update and rename lambda_function_should_use_latest_runtime.sql to lambda_function_use_latest_runtime.sql

--- query/lambda/lambda_function_use_latest_runtime.sql
+++ query/lambda/lambda_function_use_latest_runtime.sql
@@ -6,8 +6,8 @@ select
     else 'alarm'
   end as status,
   case
-    when runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') then title || ' configured with latest runtimes.'
-    else title || ' not configured with latest runtimes.'
+    when runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') then title || ' uses latest runtimes.'
+    else title || ' uses latest runtimes.'
   end as reason,
   -- Additional Dimensions
   region,
