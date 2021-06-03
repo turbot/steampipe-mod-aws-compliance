4196d9ab83f32729ee3e2e77b830507b9c4b8aac
cbruno10 <cody@turbot.com>
2021-06-03 15:41:49 -0400

    Update secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql

--- query/secretsmanager/secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql
+++ query/secretsmanager/secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql
@@ -4,9 +4,9 @@ select
   case
     when rotation_rules is null then 'alarm' -- Rotation not enabled
     when last_rotated_date is null
-    and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'ok' -- / Its scheduled but not rotated.
+    and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'ok' -- Scheduled but not rotated
     when rotation_rules is not null and last_rotated_date is null
-    and (date(current_date) - date(created_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- / Not rotated as per schedule.
+    and (date(current_date) - date(created_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- Not rotated as per schedule
   end as status,
   case
     when rotation_rules is null then  title || ' rotation not enabled.'
