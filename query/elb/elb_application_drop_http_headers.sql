e1e2c6226a8841b8c47e807aea93641e7b2cb9ac
cbruno10 <cody@turbot.com>
2021-06-03 15:37:06 -0400

    Update elb_application_drop_http_headers.sql

--- query/elb/elb_application_drop_http_headers.sql
+++ query/elb/elb_application_drop_http_headers.sql
@@ -2,12 +2,10 @@ select
   -- Required Columns
   arn as resource,
   case
-    when region in ('af-south-1', 'ap-northeast-3', 'eu-south-1') then 'skip'
     when load_balancer_attributes @> '[{"Key": "routing.http.drop_invalid_header_fields.enabled", "Value": "true"}]' then 'ok'
     else 'alarm'
   end as status,
   case
-    when region in ('af-south-1', 'ap-northeast-3', 'eu-south-1') then title || ' excluded due to unsupported control region.'
     when load_balancer_attributes @> '[{"Key": "routing.http.drop_invalid_header_fields.enabled", "Value": "true"}]' then title || ' configured to drop http headers.'
     else title || ' not configured to drop http headers.'
   end as reason,
