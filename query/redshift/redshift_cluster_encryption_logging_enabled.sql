f17d1c40aac5b10a839eb5b8e1a941de17d9e790
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-06-04 13:35:34 +0530

    Updated query

--- query/redshift/redshift_cluster_encryption_logging_enabled.sql
+++ query/redshift/redshift_cluster_encryption_logging_enabled.sql
@@ -3,12 +3,12 @@ select
   arn as resource,
   case
     when not encrypted then 'alarm'
-    when not (logging_status ->> 'LoggingEnabled')::boolean then 'alarm'
+    when not (logging_status ->> 'LoggingEnabled') :: boolean then 'alarm'
     else 'ok'
   end as status,
   case
     when not encrypted then title || ' not encrypted.'
-    when not (logging_status ->> 'LoggingEnabled')::boolean then title || ' audit logging not enabled.'
+    when not (logging_status ->> 'LoggingEnabled') :: boolean then title || ' audit logging not enabled.'
     else title || ' audit logging and encryption enabled.'
   end as reason,
   -- Additional Dimensions
