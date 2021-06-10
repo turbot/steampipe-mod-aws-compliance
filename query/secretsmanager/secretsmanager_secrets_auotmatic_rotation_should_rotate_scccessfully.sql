4a198b5aaa7b8845a11f8cc2d2448e0ea89778f4
cbruno10 <cody@turbot.com>
2021-06-10 14:58:31 -0400

    Update secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql

--- query/secretsmanager/secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql
+++ query/secretsmanager/secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql
@@ -12,8 +12,8 @@ select
      and (date(current_date) - date(last_rotated_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- The case where the secret with last_rotated_date and the diff from current minus last rotated date is exceeding from the rotation rule set days
   end as status,
   case
-    when primary_region is not null and region != primary_region then ' Replica one.'
-    when rotation_rules is null then  title || ' rotation not enabled.'
+    when primary_region is not null and region != primary_region then title || ' is a replica.'
+    when rotation_rules is null then title || ' rotation not enabled.'
     when last_rotated_date is null
       and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then title || ' scheduled but not rotated.'
     when rotation_rules is not null and last_rotated_date is null
