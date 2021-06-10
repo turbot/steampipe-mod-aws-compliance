cbcdc34d4d985ea4aa2773ca873146b8b8b3968a
cbruno10 <cody@turbot.com>
2021-06-10 15:12:48 -0400

    Update secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql

--- query/secretsmanager/secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql
+++ query/secretsmanager/secretsmanager_secrets_auotmatic_rotation_should_rotate_scccessfully.sql
@@ -2,21 +2,21 @@ select
   -- Required Columns
   arn as resource,
   case
-    when primary_region is not null and region != primary_region then 'skip'
+    when primary_region is not null and region != primary_region then 'skip' -- Replica secret
     when rotation_rules is null then 'alarm' -- Rotation not enabled
     when last_rotated_date is null
-      and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'ok' -- Scheduled but not rotated
+      and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'ok' -- New secret not due for rotation yet
     when last_rotated_date is null
-      and (date(current_date) - date(created_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- Not rotated as per schedule
+      and (date(current_date) - date(created_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- New secret overdue for rotation
     when last_rotated_date is not null
-     and (date(current_date) - date(last_rotated_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- The case where the secret with last_rotated_date and the diff from current minus last rotated date is exceeding from the rotation rule set days
+     and (date(current_date) - date(last_rotated_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then 'alarm' -- Secret has been rotated before but is overdue for another rotation
   end as status,
   case
     when primary_region is not null and region != primary_region then title || ' is a replica.'
     when rotation_rules is null then title || ' rotation not enabled.'
     when last_rotated_date is null
-      and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then title || ' scheduled but not rotated.'
-    when rotation_rules is not null and last_rotated_date is null
+      and (date(current_date) - date(created_date)) <= (rotation_rules -> 'AutomaticallyAfterDays')::integer then title || ' scheduled for rotation.'
+    when last_rotated_date is null
      and (date(current_date) - date(created_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then title || ' not rotated as per schedule.'
     when last_rotated_date is not null
       and (date(current_date) - date(last_rotated_date)) > (rotation_rules -> 'AutomaticallyAfterDays')::integer then title || ' not rotated as per schedule.'
