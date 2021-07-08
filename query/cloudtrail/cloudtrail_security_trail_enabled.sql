5a58c9fcca6a0ef3db1fafadff6d1ed6a1c91584
cbruno10 <cody@turbot.com>
2021-07-08 16:03:17 -0400

    Update cloudtrail_security_trail_enabled.sql

--- query/cloudtrail/cloudtrail_security_trail_enabled.sql
+++ query/cloudtrail/cloudtrail_security_trail_enabled.sql
@@ -54,12 +54,12 @@ select
   end as status,
   case
     when not is_logging then title || ' disabled.'
-    when not include_global_service_events then title || ' not records global service events.'
+    when not include_global_service_events then title || ' not recording global service events.'
     when not is_multi_region_trail then title || ' not a muti-region trail.'
     when not log_file_validation_enabled then title || ' log file validation disabled.'
     when kms_key_id is null then title || ' not encrypted with a KMS key.'
-    when not (jsonb_array_length(event_selectors) = 1 and event_selectors @> '[{"ReadWriteType":"All"}]') then title || ' not records events for both reads and writes.'
-    when not (event_selectors @> '[{"IncludeManagementEvents":true}]') then title || ' not records management events.'
+    when not (jsonb_array_length(event_selectors) = 1 and event_selectors @> '[{"ReadWriteType":"All"}]') then title || ' not recording events for both reads and writes.'
+    when not (event_selectors @> '[{"IncludeManagementEvents":true}]') then title || ' not recording management events.'
     when jsonb_array_length(excludeManagementEventSources) > 0 then title || ' excludes management events for ' || trim(excludeManagementEventSources::text, '[]') || '.'
     else title || ' meets all security best practices.'
   end as reason,
