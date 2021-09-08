eb1cd59b3b720a2664f9efb8fbc7a30f4e2d9da5
Ved misra <47312748+misraved@users.noreply.github.com>
2021-09-08 19:37:01 +0530

    Update the formatting of the query

--- query/elasticbeanstalk/elastic_beanstalk_enhanced_health_reporting_enabled.sql
+++ query/elasticbeanstalk/elastic_beanstalk_enhanced_health_reporting_enabled.sql
@@ -2,15 +2,11 @@ select
   -- Required Columns
   application_name as resource,
   case
-    when health_status is not null and 
-    health is not null 
-    then 'ok'
+    when health_status is not null and health is not null then 'ok'
     else 'alarm'
   end as status,
   case
-    when health_status is not null and 
-    health is not null 
-    then application_name || ' enhanced health check enabled.'
+    when health_status is not null and health is not null then application_name || ' enhanced health check enabled.'
     else application_name || ' enhanced health check disabled.'
   end as reason,
   -- Additional Dimensions
