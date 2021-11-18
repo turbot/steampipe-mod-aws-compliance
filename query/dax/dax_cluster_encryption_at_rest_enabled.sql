5352a0d427280a8b85299e629dbcf2866e2eaad8
Khushboo <khushboo@turbot.com>
2021-11-18 18:28:53 +0530

    Control dax_cluster_encryption_at_rest_enabled failing due to un-supported region issue. Closes #321

--- query/dax/dax_cluster_encryption_at_rest_enabled.sql
+++ query/dax/dax_cluster_encryption_at_rest_enabled.sql
@@ -13,4 +13,8 @@ select
   region,
   account_id
 from
-  aws_dax_cluster;
+  aws_dax_cluster
+-- DAX is not supported in all the regions, for more information please check the below link
+-- https://aws.amazon.com/about-aws/whats-new/2018/04/amazon-dynamodb-accelerator-regional-expansion/
+where
+  region in('us-east-1','us-east-2','us-west-1','us-west-2','sa-east-1','eu-west-1','ap-southeast-1','ap-northeast-1','ap-southeast-2','ap-south-1');
