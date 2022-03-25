3ad565ddcb9abf220be9546400142ee5f52911eb
misraved <ved@turbot.com>
2022-03-25 21:45:33 +0530

    Updatew the description of the query

--- query/dynamodb/dynamodb_table_auto_scaling_enabled.sql
+++ query/dynamodb/dynamodb_table_auto_scaling_enabled.sql
@@ -16,7 +16,7 @@ select
     else 'ok'
   end as status,
   case
-    when d.billing_mode = 'PAY_PER_REQUEST' then d.title || ' On-demand mode enabled.'
+    when d.billing_mode = 'PAY_PER_REQUEST' then d.title || ' on-demand mode enabled.'
     when t.resource_id is null then d.title || ' autoscaling not enabled.'
     when t.count < 2 then d.title || ' auto scaling not enabled for both read and write capacity.'
     else d.title || ' autoscaling enabled for both read and write capacity.'
