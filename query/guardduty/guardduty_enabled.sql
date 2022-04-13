e622501f301ceaaf43a726917f4adcd3aaa55c3d
rajmohanty17 <raj@turbot.com>
2022-04-13 16:17:36 +0530

    Split GuardDuty Checks for Foundational Security into Standard GuardDuty Enabled and S3 Logging Enabled Closes #357

--- query/guardduty/guardduty_enabled.sql
+++ query/guardduty/guardduty_enabled.sql
@@ -3,15 +3,15 @@ select
   'arn:' || r.partition || ':guardduty:' || r.region || ':' || r.account_id || ':detector/' || detector_id as resource,
   case
     when a.region = any (
-      ARRAY ['af-south-1', 'eu-south-1', 'cn-north-1', 'cn-northwest-1', 'me-south-1', 'us-gov-east-1']
+      ARRAY ['af-south-1', 'ap-northeast-3', 'ap-southeast-3', 'eu-south-1', 'cn-north-1', 'cn-northwest-1', 'me-south-1', 'us-gov-east-1']
     ) then 'skip'
-    when status = 'ENABLED' and data_sources -> 'S3Logs' ->> 'Status' = 'ENABLED' then 'ok'
+    when status = 'ENABLED' then 'ok'
     else 'alarm'
   end as status,
   case
-    when a.region = any ( ARRAY ['af-south-1', 'eu-south-1', 'cn-north-1', 'cn-northwest-1', 'me-south-1', 'us-gov-east-1'] ) then 'Region not supported.'
+    when a.region = any ( ARRAY ['af-south-1', 'ap-northeast-3', 'ap-southeast-3', 'eu-south-1', 'cn-north-1', 'cn-northwest-1', 'me-south-1', 'us-gov-east-1'] ) then 'Region not supported.'
     when status is null then 'No guardduty detector found.'
-    when status = 'ENABLED' and data_sources -> 'S3Logs' ->> 'Status' = 'ENABLED' then r.title || ' enabled.'
+    when status = 'ENABLED' then r.title || ' enabled.'
     else r.title || ' disabled.'
   end as reason,
   -- Additional Dimensions
