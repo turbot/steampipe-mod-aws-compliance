145a64bd3b36175249d9239b66137e4f17d2807b
Gernot Glawe <gglawe@tecracer.de>
2022-04-24 10:31:33 +0200

    handle custom runtimes

--- query/lambda/lambda_function_use_latest_runtime.sql
+++ query/lambda/lambda_function_use_latest_runtime.sql
@@ -2,11 +2,12 @@ select
   -- Required Columns
   arn as resource,
   case
-    when runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') then 'ok'
+    when (( runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') ) OR runtime is NULL) then 'ok'
     else 'alarm'
   end as status,
   case
     when runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') then title || ' uses latest runtime - ' || runtime || '.'
+    when runtime is NULL then title || ' uses custom runtime.'
     else title || ' uses ' || runtime || ' which is not the latest version.'
   end as reason,
   -- Additional Dimensions
