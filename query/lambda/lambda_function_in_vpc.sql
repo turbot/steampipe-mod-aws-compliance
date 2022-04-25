06d3b35168c4ad02c061d8311c60e0fddd7ad2f0
Gernot Glawe <gglawe@tecracer.de>
2022-04-25 19:12:30 +0200

    Lambda.5 - query only lambda which have vpc attached

--- query/lambda/lambda_function_in_vpc.sql
+++ query/lambda/lambda_function_in_vpc.sql
@@ -2,8 +2,11 @@ select
   -- Required Columns
   arn as resource,
   case
-    when vpc_id is null then 'alarm'
-    else 'ok'
+    when vpc_id is null then 'ok'
+    else case
+      when jsonb_array_length(vpc_subnet_ids) > 1 then 'ok'
+      else 'alarm'
+    end
   end status,
   case
     when vpc_id is null then title || ' is not in VPC.'
--- query/lambda/lambda_function_use_latest_runtime.sql
+++ query/lambda/lambda_function_use_latest_runtime.sql
@@ -2,16 +2,17 @@ select
   -- Required Columns
   arn as resource,
   case
-    when (( runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') ) OR runtime is NULL) then 'ok'
+    when runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') then 'ok'
     else 'alarm'
   end as status,
   case
     when runtime in ('nodejs14.x', 'nodejs12.x', 'nodejs10.x', 'python3.8', 'python3.7', 'python3.6', 'ruby2.5', 'ruby2.7', 'java11', 'java8', 'go1.x', 'dotnetcore2.1', 'dotnetcore3.1') then title || ' uses latest runtime - ' || runtime || '.'
-    when runtime is NULL then title || ' uses custom runtime.'
     else title || ' uses ' || runtime || ' which is not the latest version.'
   end as reason,
   -- Additional Dimensions
   region,
-  account_id
+  account_id,
+  packagetype
 from
-  "aws_lambda_function";
+  "aws_lambda_function"
+where packagetype = 'Zip';
