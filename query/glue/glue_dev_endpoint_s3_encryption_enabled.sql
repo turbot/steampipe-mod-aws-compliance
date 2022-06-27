435e406b98dafd2db9711440c3cb6870b7058edc
Karan Popat <karan@turbot.com>
2022-06-27 11:18:14 +0530

    Update glue_dev_endpoint_s3_encryption_enabled.sql

--- query/glue/glue_dev_endpoint_s3_encryption_enabled.sql
+++ query/glue/glue_dev_endpoint_s3_encryption_enabled.sql
@@ -1,18 +1,18 @@
 select
-  -- Required Columns
+	-- Required Columns
 	d.arn as resource,
 	case
 		when e is not null and e ->> 'S3EncryptionMode' != 'DISABLED' then 'ok'
 		else 'alarm'
 	end as "status",
-  case
-    when e is not null and e ->> 'S3EncryptionMode' != 'DISABLED' then d.title || ' enabled s3 encryption'
-    else d.title || ' disabled s3 encryption'
-  end as reason,
-  -- Additional Dimensions
-  d.region,
-  d.account_id
+	case
+		when e is not null and e ->> 'S3EncryptionMode' != 'DISABLED' then d.title || ' enabled s3 encryption'
+		else d.title || ' disabled s3 encryption'
+	end as reason,
+	-- Additional Dimensions
+	d.region,
+	d.account_id
 from
 	aws_glue_dev_endpoint d
-  left join aws_glue_security_configuration s on d.security_configuration = s.name,
-  jsonb_array_elements(s.s3_encryption) e;
+	left join aws_glue_security_configuration s on d.security_configuration = s.name,
+	jsonb_array_elements(s.s3_encryption) e;
