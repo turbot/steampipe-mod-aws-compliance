52e8fcb064f4febf204baefd06a4a45d57c29943
Karan Popat <karan@turbot.com>
2022-06-27 10:43:07 +0530

    Add extra checks glue queries

---
+++ query/glue/glue_data_catalog_encryption_settings_password_encryption_enabled.sql
@@ -0,0 +1,14 @@
+select
+	case
+		when connection_password_encryption is not null and connection_password_encryption ->> 'ReturnConnectionPasswordEncrypted' != 'false' then 'ok'
+		else 'alarm'
+	end as "status",
+  case
+    when connection_password_encryption is not null and connection_password_encryption ->> 'ReturnConnectionPasswordEncrypted' != 'false' then 'enabled glue data catalog connection password encryption in ' || region
+    else 'disabled glue data catalog connection password encryption in ' || region
+  end as reason,
+  -- Additional Dimensions
+  region,
+  account_id
+from
+	aws_glue_data_catalog_encryption_settings;
---
+++ query/glue/glue_job_s3_encryption_enabled.sql
@@ -0,0 +1,17 @@
+select
+	j.arn as resource,
+	case
+		when e is not null and e ->> 'S3EncryptionMode' != 'DISABLED' then 'ok'
+		else 'alarm'
+	end as "status",
+  case
+    when e is not null and e ->> 'S3EncryptionMode' != 'DISABLED' then j.title || ' enabled s3 encryption'
+    else j.title || ' disabled s3 encryption'
+  end as reason,
+  -- Additional Dimensions
+  j.region,
+  j.account_id
+from
+	aws_glue_job j
+  left join aws_glue_security_configuration s on j.security_configuration = s.name,
+  jsonb_array_elements(s.s3_encryption) e;
