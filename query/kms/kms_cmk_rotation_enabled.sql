f1502f56458f63f57dca3551b014714d8c96131b
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-07-15 09:20:39 -0500

    Also handle KMS keys that are disabled

--- query/kms/kms_cmk_rotation_enabled.sql
+++ query/kms/kms_cmk_rotation_enabled.sql
@@ -4,12 +4,14 @@ select
   case
     when origin = 'EXTERNAL' then 'skip'
     when key_state = 'PendingDeletion' then 'skip'
+    when key_state = 'Disabled' then 'skip'
     when not key_rotation_enabled then 'alarm'
     else 'ok'
   end as status,
   case
     when origin = 'EXTERNAL' then title || ' has imported key material.'
     when key_state = 'PendingDeletion' then title || ' is pending deletion.'
+    when key_state = 'Disabled' then title || ' is disabled.'
     when not key_rotation_enabled then title || ' key rotation disabled.'
     else title || ' key rotation enabled.'
   end as reason,
