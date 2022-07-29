221744bedac889d2cf568e241888a8db12a3f639
rajmohanty17 <raj@turbot.com>
2022-07-29 13:21:20 +0530

    Updated query to evaluated account-wise

--- query/cloudtrail/cloudtrail_enabled_all_regions.sql
+++ query/cloudtrail/cloudtrail_enabled_all_regions.sql
@@ -1,39 +1,35 @@
 with event_selectors_trail_details as (
   select
-    distinct region
+    distinct account_id
   from
     aws_cloudtrail_trail,
     jsonb_array_elements(event_selectors) as e
-  where 
+  where
     (is_logging and e ->> 'ReadWriteType' = 'All' and is_multi_region_trail)
 ),
 advanced_event_selectors_trail_details as (
   select
-    distinct region
+    distinct account_id
   from
     aws_cloudtrail_trail,
     jsonb_array_elements_text(advanced_event_selectors) as a
-  where 
-    (is_logging and advanced_event_selectors is not null and (not a like '%readOnly%'))
+  where
+    (is_multi_region_trail and is_logging and advanced_event_selectors is not null and (not a like '%readOnly%'))
 )
 select
   -- Required Columns
-  r.name as resource,
+  a.title as resource,
   case
-    when r.opt_in_status = 'not-opted-in' then 'skip'
-    when d.region is null and ad.region is null then 'alarm'
+    when d.account_id is null and ad.account_id is null then 'alarm'
     else 'ok'
   end as status,
     case
-    when r.opt_in_status = 'not-opted-in' then r.region ||  ' region not-opted-in.'
-    when d.region is null and ad.region is null then 'cloudtrail disabled.'
-    else 'cloudtrail enabled ' || r.name || '.'
+    when d.account_id is null and ad.account_id is null then 'cloudtrail disabled.'
+    else 'cloudtrail enabled.'
   end as reason,
   -- Additional Dimensions
-  r.name,
-  r.account_id
+  a.account_id
 from
-  aws_region as r
-left join event_selectors_trail_details as d on d.region = r.name
-left join advanced_event_selectors_trail_details as ad on ad.region = r.name
-order by r.name;
+  aws_account as a
+  left join event_selectors_trail_details as d on d.account_id = a.account_id
+  left join advanced_event_selectors_trail_details as ad on ad.account_id = a.account_id;
