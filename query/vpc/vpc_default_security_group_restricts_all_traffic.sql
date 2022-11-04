9cee885fc55bf1aa63f89078068201d378255cff
Lalit Bhardwaj <lalit@turbot.com>
2022-11-04 23:42:32 +0530

    Update `iam_user_in_group` and `vpc_default_security_group_restricts_all_traffic` queries to handle empty column data (#520)

--- query/vpc/vpc_default_security_group_restricts_all_traffic.sql
+++ query/vpc/vpc_default_security_group_restricts_all_traffic.sql
@@ -2,15 +2,15 @@ select
   -- Required Columns
   arn resource,
   case
-    when ip_permissions is null and ip_permissions_egress is null then 'ok'
+    when jsonb_array_length(ip_permissions) = 0 and jsonb_array_length(ip_permissions_egress) = 0 then 'ok'
     else 'alarm'
   end status,
   case
-    when ip_permissions is not null and ip_permissions_egress is not null
+    when jsonb_array_length(ip_permissions) > 0 and jsonb_array_length(ip_permissions_egress) > 0
       then 'Default security group ' || group_id || ' has inbound and outbound rules.'
-    when ip_permissions is not null and ip_permissions_egress is null
+    when jsonb_array_length(ip_permissions) > 0 and jsonb_array_length(ip_permissions_egress) = 0
       then 'Default security group ' || group_id || ' has inbound rules.'
-    when ip_permissions is null and ip_permissions_egress is not null
+    when jsonb_array_length(ip_permissions) = 0 and jsonb_array_length(ip_permissions_egress) > 0
       then 'Default security group ' || group_id || ' has outbound rules.'
     else 'Default security group ' || group_id || ' has no inbound or outbound rules.'
   end reason,
