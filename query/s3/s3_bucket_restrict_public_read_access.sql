923093c8234fc41ba6af083693a6312c81824edd
Khushboo Sharma <khushboosharma@Khushboos-MBP.bbrouter>
2023-01-18 15:47:22 +0530

    addded bucket policy check in s3_bucket_restrict_public_read_access query

--- query/s3/s3_bucket_restrict_public_read_access.sql
+++ query/s3/s3_bucket_restrict_public_read_access.sql
@@ -1,4 +1,4 @@
-with data as (
+with public_acl as (
   select
     distinct name
   from
@@ -10,16 +10,31 @@ with data as (
       grants ->> 'Permission' = 'FULL_CONTROL'
       or grants ->> 'Permission' = 'READ_ACP'
     )
-  )
+), public_policy as (
+  select
+    distinct name
+  from
+    aws_s3_bucket,
+    jsonb_array_elements(policy_std -> 'Statement') as s,
+    jsonb_array_elements_text(s -> 'Action') as a
+  where
+    s ->> 'Effect' = 'Allow'
+    and (s -> 'Principal' -> 'AWS' = '["*"]' or s ->> 'Principal' = '*')
+    and
+      (a = '*'
+      or a = 's3:*'
+      or a ilike 's3:Get%'
+      or a ilike 's3:Describe%')
+)
 select
   -- Required Columns
   b.arn as resource,
   case
-    when d.name is null then 'ok'
+    when (block_public_acls or a.name is null) and (block_public_policy or p.name is null) then 'ok'
     else 'alarm'
   end status,
   case
-    when d.name is null then b.title || ' not publicly readable.'
+    when (block_public_acls or a.name is null ) and (block_public_policy or p.name is null) then b.title || ' not publicly readable.'
     else b.title || ' publicly readable.'
   end reason,
   -- Additional Dimensions
@@ -27,4 +42,5 @@ select
   b.account_id
 from
   aws_s3_bucket as b
-  left join data as d on b.name = d.name;
+  left join public_acl as a on b.name = a.name
+  left join public_policy as p on b.name = p.name;
--- query/s3/s3_bucket_restrict_public_write_access.sql
+++ query/s3/s3_bucket_restrict_public_write_access.sql
@@ -1,4 +1,4 @@
-with data as (
+with public_acl as (
   select
     distinct name
   from
@@ -10,16 +10,27 @@ with data as (
       grants ->> 'Permission' = 'FULL_CONTROL'
       or grants ->> 'Permission' = 'WRITE_ACP'
     )
-  )
+), public_policy as (
+  select
+    distinct name
+  from
+    aws_s3_bucket,
+    jsonb_array_elements(policy_std -> 'Statement') as s,
+    jsonb_array_elements_text(s -> 'Action') as a
+  where
+    s ->> 'Effect' = 'Allow'
+    and (s -> 'Principal' -> 'AWS' = '["*"]' or s ->> 'Principal' = '*')
+    and (a = '*' or a = 's3:*')
+)
 select
   -- Required Columns
   b.arn as resource,
   case
-    when d.name is null then 'ok'
+    when (block_public_acls or a.name is null) and (block_public_policy or p.name is null) then 'ok'
     else 'alarm'
   end status,
   case
-    when d.name is null then b.title || ' not publicly writable.'
+    when (block_public_acls or a.name is null) and (block_public_policy or p.name is null) then b.title || ' not publicly writable.'
     else b.title || ' publicly writable.'
   end reason,
   -- Additional Dimensions
@@ -27,4 +38,5 @@ select
   b.account_id
 from
   aws_s3_bucket as b
-  left join data as d on b.name = d.name;
+  left join public_acl as a on b.name = a.name
+  left join public_policy as p on b.name = p.name;
