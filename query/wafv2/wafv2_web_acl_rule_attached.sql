53b2ae26d32a5980b525fde5de45a43fbfb85b25
Khushboo Sharma <khushboosharma@Khushboos-MBP.bbrouter>
2023-01-19 20:08:15 +0530

    update reason for query wafv2_web_acl_rule_attached.sql

--- query/wafv2/wafv2_web_acl_rule_attached.sql
+++ query/wafv2/wafv2_web_acl_rule_attached.sql
@@ -1,3 +1,14 @@
+with rule_group_count as (
+  select
+    count(*) as rule_group_count
+  from
+    aws_wafv2_web_acl,
+    jsonb_array_elements(rules) as r
+  where
+    r -> 'Statement' -> 'RuleGroupReferenceStatement' ->> 'ARN' is not null
+  group by
+    arn
+)
 select
   -- Required Columns
   arn as resource,
@@ -7,7 +18,7 @@ select
   end as status,
   case
     when rules is null or jsonb_array_length(rules) = 0 then title || ' has no attached rules.'
-    else title || ' has attached rules.'
+    else title || ' has ' || (select rule_group_count from rule_group_count ) || ' rule group(s) and ' || (jsonb_array_length(rules) - (select rule_group_count from rule_group_count )) || ' rule(s) attached.'
   end as reason,
   -- Additional Dimensions
   region,
