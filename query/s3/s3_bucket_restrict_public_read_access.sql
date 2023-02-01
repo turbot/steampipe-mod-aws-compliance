77d79d367c7d9dc0cf865e281bdd50fc969a2359
khushboo9024 <khushboo@turbot.com>
2023-02-01 20:51:34 +0530

    update

--- query/s3/s3_bucket_restrict_public_read_access.sql
+++ query/s3/s3_bucket_restrict_public_read_access.sql
@@ -9,17 +9,42 @@ with public_acl as (
     and (
       grants ->> 'Permission' = 'FULL_CONTROL'
       or grants ->> 'Permission' = 'READ_ACP'
+      or grants ->> 'Permission' = 'READ'
     )
+  ),read_access_policy as (
+    select
+      distinct name
+    from
+      aws_s3_bucket,
+      jsonb_array_elements(policy_std -> 'Statement') as s,
+      jsonb_array_elements_text(s -> 'Action') as action
+    where
+      s ->> 'Effect' = 'Allow'
+      and (
+        s -> 'Principal' -> 'AWS' = '["*"]'
+        or s ->> 'Principal' = '*'
+      )
+      and (
+        action = '*'
+        or action = '*:*'
+        or action = 's3:*'
+        or action ilike 's3:get%'
+        or action ilike 's3:describe%'
+      )
 )
 select
   -- Required Columns
   b.arn as resource,
   case
-    when (block_public_acls or a.name is null) and block_public_policy then 'ok'
+    when (block_public_acls or a.name is null) and not bucket_policy_is_public then 'ok'
+    when bucket_policy_is_public and block_public_policy then 'ok'
+    when bucket_policy_is_public and p.name is null then 'ok'
     else 'alarm'
   end status,
   case
-    when (block_public_acls or a.name is null ) and block_public_policy then b.title || ' not publicly readable.'
+    when (block_public_acls or a.name is null) and not bucket_policy_is_public then b.title || ' not publicly readable.'
+    when bucket_policy_is_public and block_public_policy then b.title || ' not publicly readable.'
+    when bucket_policy_is_public and p.name is null then  b.title || ' not publicly readable.'
     else b.title || ' publicly readable.'
   end reason,
   -- Additional Dimensions
@@ -27,4 +52,5 @@ select
   b.account_id
 from
   aws_s3_bucket as b
-  left join public_acl as a on b.name = a.name;
+  left join public_acl as a on b.name = a.name
+  left join read_access_policy as p on b.name = p.name;
--- query/s3/s3_bucket_restrict_public_write_access.sql
+++ query/s3/s3_bucket_restrict_public_write_access.sql
@@ -9,17 +9,46 @@ with public_acl as (
     and (
       grants ->> 'Permission' = 'FULL_CONTROL'
       or grants ->> 'Permission' = 'WRITE_ACP'
+      or grants ->> 'Permission' = 'WRITE'
     )
+), write_access_policy as (
+    select
+      distinct name
+    from
+      aws_s3_bucket,
+      jsonb_array_elements(policy_std -> 'Statement') as s,
+      jsonb_array_elements_text(s -> 'Action') as action
+    where
+      s ->> 'Effect' = 'Allow'
+      and (
+        s -> 'Principal' -> 'AWS' = '["*"]'
+        or s ->> 'Principal' = '*'
+      )
+      and (
+        action = '*'
+        or action = '*:*'
+        or action = 's3:*'
+        or action ilike 's3:put%'
+        or action ilike 's3:delete%'
+        or action ilike 's3:create%'
+        or action ilike 's3:update%'
+        or action ilike 's3:replicate%'
+        or action ilike 's3:restore%'
+      )
 )
 select
   -- Required Columns
   b.arn as resource,
+  bucket_policy_is_public,
   case
-    when (block_public_acls or a.name is null) and block_public_policy then 'ok'
+    when (block_public_acls or a.name is null) and not bucket_policy_is_public then 'ok'
+    when bucket_policy_is_public and block_public_policy then 'ok'
+    when bucket_policy_is_public and p.name is null then 'ok'
     else 'alarm'
   end status,
   case
-    when (block_public_acls or a.name is null) and block_public_policy then b.title || ' not publicly writable.'
+    when (block_public_acls or a.name is null ) and not bucket_policy_is_public then b.title || ' not publicly writable.'
+    when bucket_policy_is_public and block_public_policy then b.title || ' not publicly writable.'
     else b.title || ' publicly writable.'
   end reason,
   -- Additional Dimensions
@@ -27,4 +56,5 @@ select
   b.account_id
 from
   aws_s3_bucket as b
-  left join public_acl as a on b.name = a.name;
+  left join public_acl as a on b.name = a.name
+  left join write_access_policy as p on b.name = p.name
