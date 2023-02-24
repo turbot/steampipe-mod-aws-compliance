06788df58010cdd4f510b1874a1798c1dc68220d
madhushreeray30 <madhushree@turbot.com>
2023-02-24 17:21:53 +0530

    update query

--- query/ecs/ecs_service_load_balancer_attached.sql
+++ query/ecs/ecs_service_load_balancer_attached.sql
@@ -1,12 +1,13 @@
 select
   -- Required Columns
-    arn as resource,
+  arn as resource,
   case
-    when jsonb_array_length(load_balancers)= 0 then 'alarm'
+    when jsonb_array_length(load_balancers) = 0 then 'alarm'
     else 'ok'
   end as status,
   case
-    when jsonb_array_length(load_balancers)= 0 then title || ' has no load balancers attached.' else title || ' has ' || jsonb_array_length(load_balancers) || ' load balancers attached.'
+    when jsonb_array_length(load_balancers) = 0 then title || ' has no load balancer attached.'
+    else title || ' has ' || jsonb_array_length(load_balancers) || ' load balancer(s) attached.'
   end as reason,
   -- Additional Dimensions
   region,
