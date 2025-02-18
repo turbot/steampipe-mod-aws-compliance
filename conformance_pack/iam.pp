55f789eb808970861e7c10b506c932c78e9fdd48
Sergey Zelensky <108680000+ramses999@users.noreply.github.com>
2025-02-18 15:58:51 +0200

    Fix typo in iam_user_hardware_mfa_enabled query (#851)

--- conformance_pack/iam.pp
+++ conformance_pack/iam.pp
@@ -1419,7 +1419,7 @@ query "iam_user_hardware_mfa_enabled" {
         else 'ok'
       end as status,
       case
-        when serial_number is null then u.name || ' MFA device not configured.'
+        when serial_number is null then u.name || ' MFA hardware device not configured.'
         when serial_number like any(array['%mfa%','%sms-mfa%']) then u.name || ' MFA enabled, but the MFA associated is a virtual device.'
         else u.name || ' hardware MFA device enabled.'
       end as reason
