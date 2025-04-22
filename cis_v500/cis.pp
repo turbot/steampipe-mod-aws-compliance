4932b669788d424780a20d57bc95eb6fa3a00191
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2025-04-22 11:44:25 +0530

    Add initial cisv500

---
+++ cis_v500/cis.pp
@@ -0,0 +1,23 @@
+locals {
+  cis_v500_common_tags = merge(local.aws_compliance_common_tags, {
+    cis         = "true"
+    cis_version = "v4.0.0"
+  })
+}
+
+benchmark "cis_v500" {
+  title         = "CIS v4.0.0"
+  description   = "The CIS Amazon Web Services Foundations Benchmark provides prescriptive guidance for configuring security options for a subset of Amazon Web Services with an emphasis on foundational, testable, and architecture agnostic settings."
+  documentation = file("./cis_v500/docs/cis_overview.md")
+  children = [
+    benchmark.cis_v500_1,
+    benchmark.cis_v500_2,
+    benchmark.cis_v500_3,
+    benchmark.cis_v500_4,
+    benchmark.cis_v500_5
+  ]
+
+  tags = merge(local.cis_v500_common_tags, {
+    type = "Benchmark"
+  })
+}
