4692a016b822881ffe5e1d750829ead76a24c923
khushboosharma <khushboo@turbot.com>
2025-07-24 11:33:24 +0530

    Access Key Rotation Scan Should Exclude Inactive Keys closes #909

--- conformance_pack/iam.pp
+++ conformance_pack/iam.pp
@@ -940,13 +940,16 @@ query "iam_user_access_key_age_90" {
     select
       'arn:' || partition || ':iam::' || account_id || ':user/' || user_name || '/accesskey/' || access_key_id as resource,
       case
+        when status <> 'Active' then 'skip'
         when create_date <= (current_date - interval '90' day) then 'alarm'
         else 'ok'
       end status,
-      user_name || ' ' || access_key_id || ' created ' || to_char(create_date , 'DD-Mon-YYYY') ||
+      case
+        when status <> 'Active' then user_name ||' ' || access_key_id || ' status is ' || status || '.'
+        else user_name || ' ' || access_key_id || ' created ' || to_char(create_date , 'DD-Mon-YYYY') ||
         ' (' || extract(day from current_timestamp - create_date) || ' days).'
-      as reason
-      ${local.common_dimensions_global_sql}
+      end as reason
+      --${local.common_dimensions_global_sql}
     from
       aws_iam_access_key;
   EOQ
